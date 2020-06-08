de24c98cab1028a0ec4482c5fecda2194e409c74
przemskydev <przemekgithub@o2.pl>
2020-06-08 20:39:40 +0200

    async await

--- src/iterating.mjs
+++ src/iterating.mjs
@@ -1,16 +1,48 @@
-import setText , {appendText} from './results.mjs';
+import setText, { appendText } from './results.mjs';
 
-export function get(){
+export async function get() {
+  const { data } = await axios.get("http://localhost:3000/orders/1");
+  console.log(data)
 }
 
-export function getCatch(){
+export async function getCatch() {
+  try {
+    const { data } = await axios.get("http://localhost:3000/orders/1");
+    console.log(data)
+  } catch (err) {
+    console.log(err)
+  }
 }
 
-export function chain(){
+export async function chain() {
+  const { data } = await axios.get("http://localhost:3000/orders/1");
+  const { data: address } = await axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`);
+
+  console.log(`City: ${address.city}`)
 }
 
-export function concurrent(){
+export async function concurrent() {
+  const orderStatus = axios.get("http://localhost:3000/orderStatuses");
+  const orders = axios.get("http://localhost:3000/orders");
+
+  const { data: statuses } = await orderStatus;
+  const { data: order } = await orders;
+
+  console.log(statuses);
+  console.log(order[0])
 }
 
-export function parallel(){
+export async function parallel() {
+  console.log('')
+
+  await Promise.all([
+    (async () => {
+      const {data} = await axios.get("http://localhost:3000/orderStatuses")
+      console.log(data)
+    })(),
+    (async () => {
+      const {data} = await axios.get("http://localhost:3000/orders")
+      console.log(data)
+    })(),
+  ])
 }
